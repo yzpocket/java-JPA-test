c70b71e4c4330083cc9481f82d5931edab94f5d8
INYONG KIM <ohnyong87@gmail.com>
2023-09-01 21:30:27 +0900

    test : Persistence -> action queue(commit) Test
    * JPA has persistence algorithm
    - EMF makes EM
    - Entity Manager management Entities and Database
    - EM has Transaction algorithm for control
    - Transaction is kind of temporary repository to go
    - When Action Queue flags 'commit', EM allows CRUD to DB

--- src/main/java/com/sparta/entity/Memo.java
+++ src/main/java/com/sparta/entity/Memo.java
@@ -5,8 +5,9 @@ import jakarta.persistence.*;
 @Entity //(name = "Memo") // JPA가 관리할 수 있는 Entity 클래스 지정 //@Entity(name = "")작성안하면 기본 클래스이름 따라감.
 @Table(name = "memo") // 매핑할 테이블의 이름을 지정
 public class Memo {
+
+    //@GeneratedValue(strategy = GenerationType.IDENTITY) //<-- Autoincrement 해주는것. //테스트에서는 Autoincrement 기능 잠시 꺼둠
     @Id //idx로 알고 있었던 부분임. pk 속성을 가지고 있음.
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //<-- Autoincrement 해주는것.
     private Long id;
 
     // nullable: null 허용 여부
@@ -17,4 +18,32 @@ public class Memo {
     // length: 컬럼 길이 지정
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+
+    // ***WARNING*** Entity에서는 Getter,Setter를 매우 조심해야 한다.
+    // 무결성, 정합성을 유지하는데 다른 실수에 따라 Setter로 다른 값이 입력될 가능성이 있기때문,
+    // 학습중이니 상관 없지만 실무에서는 신경써야 함
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
--- src/test/java/EntityTest.java
+++ src/test/java/EntityTest.java
@@ -1,10 +1,16 @@
+import com.sparta.entity.Memo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EntityTest {
+    //트랜잭션은 DB데이터들의 무결성과 정합성을 유지하기 위한 논리적 개념
+    //SQL중 단 하나라도 실패하면 모든 변경을 되돌리는것(커밋을 해야 DB에 반영 됨)
+    //JPA는 이런 영속성 컨텍스트로 관리하는 개념을 도입했다.
 
     EntityManagerFactory emf;
     EntityManager em;
@@ -16,7 +22,30 @@ public class EntityTest {
     }
 
     @Test
+    @DisplayName("EntityTransaction 성공 테스트")
     void test1() {
+        EntityTransaction et = em.getTransaction(); // [1] EntityManager 에서 EntityTransaction 을 가져옵니다.
 
+        et.begin(); // [2] 트랜잭션을 시작합니다.
+        //영속성 컨텍스트(임시저장소같은느낌) 시작. -> action queue인 Commit 전까지 임시보호소 대기
+        try { // DB 작업을 수행합니다.
+
+            Memo memo = new Memo(); // 저장할 Entity 객체를 생성합니다.
+            memo.setId(1L); // 식별자 값을 넣어줍니다.
+            memo.setUsername("Robbie");
+            memo.setContents("영속성 컨텍스트와 트랜잭션 이해하기");
+
+            em.persist(memo); // EntityManager 사용하여 memo 객체를 영속성 컨텍스트에 저장합니다.
+
+            et.commit(); // 오류가 발생하지 않고 정상적으로 수행되었다면 commit 을 호출합니다.
+            // commit 이 호출되면서 DB 에 수행한 DB 작업들이 반영됩니다.
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            et.rollback(); // DB 작업 중 오류 발생 시 rollback 을 호출합니다.
+        } finally {
+            em.close(); // 사용한 EntityManager 를 종료합니다.
+        }
+
+        emf.close(); // 사용한 EntityManagerFactory 를 종료합니다.
     }
 }
